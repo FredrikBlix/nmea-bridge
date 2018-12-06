c4a22211bdfc7a7526a8e68edef164d18f417915
Fredrik Blix <fredrik.blix@coromatic.se>
2018-12-06 12:15:20 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-/*
-* The main script just starts the bridge
-*/
+/**
+*   This file is part of nmea-bridge.
+*
+*   nmea-bridge is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Lesser General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   nmea-bridge is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU Lesser General Public License for more details.
+*
+*   You should have received a copy of the GNU Lesser General Public License
+*   along with nmea-bridge.  If not, see <https://www.gnu.org/licenses/>.
+*/    
 
 const bridge = require('./nmea-bridge');
 const config = require('config');
